4ed657ec2feabb953c6b0da2725809e5a253a1b9
xiaoxiang47 <1815724904@qq.com>
2023-11-24 17:35:41 +0800

    前端数据字典展示

--- src/views/dict/list.vue
+++ src/views/dict/list.vue
@@ -2,9 +2,11 @@
   <div class="app-container">
     <div class="el-toolbar">
       <div class="el-toolbar-body" style="justify-content: flex-start">
-        <el-button type="text" @click="exportData"
-          ><i class="fa fa-plus" /> 导出</el-button
-        >
+        <a href="http://localhost:8202/admin/cmn/dict/exportData" target="_blank">
+            <el-button type="text"
+          ><i class="fa fa-plus" /> 导出</el-button>
+        </a>
+        <el-button type="text" @click="importData"><i class="fa fa-plus"/> 导入</el-button>
       </div>
     </div>
     <el-table
@@ -22,6 +24,24 @@
       <el-table-column prop="value" label="值" width="150"> </el-table-column>
       <el-table-column prop="createTime" label="创建时间"> </el-table-column>
     </el-table>
+
+    <el-dialog title="导入" :visible.sync="dialogImportVisible" width="480px">
+    <el-form label-position="right" label-width="170px">
+        <el-form-item label="文件">
+            <el-upload
+                       :multiple="false"
+                       :on-success="onUploadSuccess"
+                       :action="'http://localhost:8202/admin/cmn/dict/importData'"
+                       class="upload-demo">
+                <el-button size="small" type="primary">点击上传</el-button>
+                <div slot="tip" class="el-upload__tip">只能上传excel文件，且不超过500kb</div>
+            </el-upload>
+        </el-form-item>
+    </el-form>
+    <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogImportVisible = false">取消</el-button>
+    </div>
+</el-dialog>
   </div>
 </template>
 
@@ -30,17 +50,23 @@ import dict from "@/api/dict";
 export default {
   data() {
     return {
+      dialogImportVisible: false,  //设置弹框是否弹出
       list: [], //数据字典列表数组
     };
   },
   created() {
     this.getDictList(1);
-    this.$message({
-      type: "success",
-      message: "修改医院设置成功!",
-    });
   },
   methods: {
+    //导入数据字典
+    importData(){
+      this.dialogImportVisible = true
+    },
+    //上传成功调用的方法
+    onUploadSuccess(){
+      this.dialogImportVisible = false
+      this.getDictList(1)
+    },
     getDictList(id) {
       dict.getDictList(id).then((response) => {
         this.list = response.data;
